cc6ffb43cc8f1493486dd1a5f033c1f339e0c143
wd_wd99 <wd_wd99@163.com>
2023-08-26 15:05:50 +0800

    2 by wd

--- src/main/java/com/tencent/wxcloudrun/entity/WXContent.java
+++ src/main/java/com/tencent/wxcloudrun/entity/WXContent.java
@@ -1,6 +1,6 @@
 package com.tencent.wxcloudrun.entity;
 
 public class WXContent {
-    public static final String APPID = "wxd49d2945192e3420"; //"wx73883a38b9ab7534";//"wxd49d2945192e3420";//"wx623f0df535467569" ;
+    public static final String APPID = "wx73883a38b9ab7534"; //"wx73883a38b9ab7534";//"wxd49d2945192e3420";//"wx623f0df535467569" ;
     public static final String APPSECRET = "36c3cc68a6409cf12532f3b5d380d4b3"; //"5aa52529e26e484c3ccb83e7be323260"; //"36c3cc68a6409cf12532f3b5d380d4b3";//""89ca08214e9092ccb9b20b70f24ced9b" ;
 }
